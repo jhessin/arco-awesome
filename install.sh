256fcfaca7e119ee94d7032b136635e17441b976
Jim Hessin <jhessin@gmail.com>
2020-02-14 15:25:35 -0700

    Update

--- install.sh
+++ install.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/env bash
 
-# First update everything
-sudo pacman -Scc --noconfirm &&
-sudo pacman -Rdd --noconfirm libdmx libxxf86dga &&
-sudo pacman -Syyu --noconfirm --needed &&
-sudo pacman -Rdd --noconfirm xorgproto
-yay -Syu --noconfirm --needed --noredownload --norebuild &&
+# first run update fix
+# by removing obsolete packages
+sudo pacman -Scc --noconfirm
+sudo pacman -Rdd --noconfirm libdmx libxxf86dga
 
+# update everything
+sudo pacman -Syyu --noconfirm --needed
+sudo pacman -Rdd --noconfirm xorgproto
+yay -Syu --noconfirm --needed --noredownload --norebuild
+
+run() {
+  echo "----====----====---====$1====----====----====----===="
+  $1
+}
 # Now install awesome
-./000-use-all-cores-makepkg-conf-v4.sh &&
-./100-display-manager-and-desktop-v1.sh &&
-./110-install-sound-v3.sh &&
-./120-bluetooth-v2.sh &&
-./121-fix-bluetooth-switch-not-working-v1.sh &&
-./130-install-printers-v3.sh &&
-./140-install-samba-v3.sh &&
-./150-install-network-discovery-v4.sh &&
-./160-install-tlp-for-laptops-v1.sh &&
-./200-software-arch-linux-repo-v2.sh &&
-./300-software-AUR-repo-v1.sh &&
-./400-software-arch-linux-repo-distro-specific-v1.sh &&
-./500-software-AUR-repo-distro-specific-v1.sh &&
-./600-software-from-ArcoLinux-repo-v1.sh &&
-./700-installing-fonts-v2.sh &&
-./800-autologin-v3.sh &&
-./900-fix-microcode-error-v1.sh &&
-./910-fix-mouse-cursor-breeze-snow-v1.sh &&
+run ./000-use-all-cores-makepkg-conf-v4.sh &&
+run ./100-display-manager-and-desktop-v1.sh &&
+run ./110-install-sound-v3.sh &&
+run ./120-bluetooth-v2.sh &&
+run ./121-fix-bluetooth-switch-not-working-v1.sh &&
+run ./130-install-printers-v3.sh &&
+run ./140-install-samba-v3.sh &&
+run ./150-install-network-discovery-v4.sh &&
+run ./160-install-tlp-for-laptops-v1.sh &&
+run ./200-software-arch-linux-repo-v2.sh &&
+run ./300-software-AUR-repo-v1.sh &&
+run ./400-software-arch-linux-repo-distro-specific-v1.sh &&
+run ./500-software-AUR-repo-distro-specific-v1.sh &&
+run ./600-software-from-ArcoLinux-repo-v1.sh &&
+run ./700-installing-fonts-v2.sh &&
+run ./800-autologin-v3.sh &&
+run ./900-fix-microcode-error-v1.sh &&
+run ./910-fix-mouse-cursor-breeze-snow-v1.sh &&
 
 echo "Be sure to run the scripts in system_setup"
