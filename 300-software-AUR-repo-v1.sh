20987fc9af79e9b983e6690285554e188180d2a3
Jim Hessin <jhessin@gmail.com>
2020-02-15 15:38:32 -0700

    commented out set -e

--- 300-software-AUR-repo-v1.sh
+++ 300-software-AUR-repo-v1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+# set -e
 echo "One or more packages are coming from AUR as source."
 echo "If a package from AUR fails to install, all other packages after that will NOT be installed."
 echo "We put the set -e for you to notice some package broke the script and you can intervene manually."
