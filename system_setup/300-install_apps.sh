f80661ec639744e0bc00195619dccdcc6a3e3424
Jim Hessin <jhessin@gmail.com>
2020-02-16 01:07:01 -0700

    added yarn to the mix

--- system_setup/300-install_apps.sh
+++ system_setup/300-install_apps.sh
@@ -14,6 +14,11 @@ get python-pip &&
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
 7D2BAF1CF37B13E2069D6956105BD0E739499BDB &&
 \curl -sSL https://get.rvm.io | bash -s stable --rails &&
+gem install neovim --user
+
+# Install yarn
+get yarn
+yarn global add neovim
 
 # i3lock and xautolock
 get i3lock &&
