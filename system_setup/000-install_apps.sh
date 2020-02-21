8f6c37471cf1755c988e8e77ed39720f96c528e4
Jim Hessin <jhessin@gmail.com>
2020-02-21 01:46:09 -0700

    install more apps

--- system_setup/000-install_apps.sh
+++ system_setup/000-install_apps.sh
@@ -18,6 +18,7 @@ gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
 [[ -s "$HOME/.mkshrc" ]] && source "$HOME/.mkshrc" # add rvm to path
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 gem install neovim
+gem install rubocop
 
 # Install yarn
 get yarn
@@ -34,3 +35,6 @@ pip install --user neovim &&
 # Install nvimpager and vimpager
 get nvimpager
 get vimpager
+
+# install yarn stuff
+yarn global add bash-language-server prettier stylelint eslint tslint
