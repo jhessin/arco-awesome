e613f0446f5aeace01a274cb27d7505e5fb01b30
Jim Hessin <jhessin@gmail.com>
2020-02-21 02:08:01 -0700

    added aurget and pacget to functions

--- functions.sh
+++ functions.sh
@@ -32,3 +32,86 @@ function clean {
   git reset --hard
   git clean -f -d
 }
+
+function pacget {
+  if pacman -Qi $1 &> /dev/null; then
+
+    tput setaf 2
+    echo "################################################################"
+    echo "################## "$package" is already installed"
+    echo "################################################################"
+    tput sgr0
+
+  else
+
+    tput setaf 3
+    echo "################################################################"
+    echo "######### Installing with pacman"
+    echo "################################################################"
+    tput sgr0
+    sudo pacman -S --noconfirm --needed $1
+  fi
+}
+function aurget {
+  package="$1"
+
+  #----------------------------------------------------------------------------------
+
+  #checking if application is already installed or else install with aur helpers
+  if pacman -Qi $package &> /dev/null; then
+
+    tput setaf 2
+    echo "################################################################"
+    echo "################## "$package" is already installed"
+    echo "################################################################"
+    tput sgr0
+
+  else
+
+  #checking which helper is installed
+  if pacman -Qi yay &> /dev/null; then
+
+    tput setaf 3
+    echo "################################################################"
+    echo "######### Installing with yay"
+    echo "################################################################"
+    tput sgr0
+
+    yay -S --noconfirm $package
+
+  elif pacman -Qi trizen &> /dev/null; then
+
+    tput setaf 3
+    echo "################################################################"
+    echo "######### Installing with trizen"
+    echo "################################################################"
+    tput sgr0
+    trizen -S --noconfirm --needed --noedit $package
+
+  fi
+
+  fi
+
+
+  # Just checking if installation was successful
+  if pacman -Qi $package &> /dev/null; then
+
+    tput setaf 2
+    echo "################################################################"
+    echo "#########  Checking ..."$package" has been installed"
+    echo "################################################################"
+    tput sgr0
+
+  else
+
+    tput setaf 1
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "!!!!!!!!!  "$package" has NOT been installed"
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    tput sgr0
+
+  fi
+
+#----------------------------------------------------------------------------------
+
+}
--- system_setup/000-install_apps.sh
+++ system_setup/000-install_apps.sh
@@ -9,6 +9,7 @@ source ../functions.sh
 # Install python
 get python &&
 get python-pip &&
+pip install --user neovim
 
 # Install rvm, ruby, rails
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
@@ -21,20 +22,25 @@ gem install neovim
 gem install rubocop
 
 # Install yarn
-get yarn
+aurget yarn &&
 yarn global add neovim
 
 # i3lock and xautolock
-get i3lock &&
-get xautolock
+aurget i3lock
+aurget xautolock
 
 # Install neovim
-get neovim &&
-pip install --user neovim &&
+aurget neovim
+
+# install synergy for mouse sharing
+pacget synergy
 
 # Install nvimpager and vimpager
-get nvimpager
-get vimpager
+aurget nvimpager
+aurget vimpager
 
 # install yarn stuff
 yarn global add bash-language-server prettier stylelint eslint tslint
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
