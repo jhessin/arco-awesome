64ab72370627ea2338a9f1d8a1cf6a82658b4a22
Jim Hessin <jhessin@gmail.com>
2020-02-21 01:12:25 -0700

    rvm is initialized automatically

--- system_setup/000-install_apps.sh
+++ system_setup/000-install_apps.sh
@@ -13,7 +13,10 @@ get python-pip &&
 # Install rvm, ruby, rails
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
 7D2BAF1CF37B13E2069D6956105BD0E739499BDB &&
-\curl -sSL https://get.rvm.io | bash -s stable --rails &&
+\curl -sSL https://get.rvm.io | bash -s stable --rails
+
+[[ -s "$HOME/.mkshrc" ]] && source "$HOME/.mkshrc" # add rvm to path
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 gem install neovim
 
 # Install yarn
