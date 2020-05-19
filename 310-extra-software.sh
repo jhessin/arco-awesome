eb8afaaf1a612b6678a72a59ee88706228f48a78
Jim Hessin <jhessin@gmail.com>
2020-05-19 05:25:33 -0500

    Updated rvm download

--- 310-extra-software.sh
+++ 310-extra-software.sh
@@ -24,9 +24,10 @@ pacget vifm
 
 echo "Installing rvm, ruby, and rails"
 
-sudo gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
-  7D2BAF1CF37B13E2069D6956105BD0E739499BDB &&
-  \sudo curl -sSL https://get.rvm.io | bash -s stable --rails
+sudo gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys \
+409B6B1796C275462A1703113804BB82D39DC0E3 \
+7D2BAF1CF37B13E2069D6956105BD0E739499BDB &&
+sudo curl -sSL https://get.rvm.io | bash -s stable --rails
 
 [[ -s "$HOME/.mkshrc" ]] && source "$HOME/.mkshrc"                   # add rvm to path
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
